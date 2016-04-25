baf686e59e92885c874e33845093b831f7dbb1b4
Sophia <sofiya.belous@gmail.com>
2016-04-25 03:45:48 +0300

    added calculating valid/invalid words

--- app/routes.js
+++ app/routes.js
@@ -1,24 +1,33 @@
 module.exports = function(app) {    
     
     app.post('/api/validation/word', function(req, res) {
-        // console.log(req.body);
         var words = req.body.words;
         
-        var resultWords = [];
+        var resultWords = [],
+            invalidWords = 0,
+            validWords = 0;
+        
         
         for (var i = 0; i < words.length; i++) {
             (function(i) {
                 var word = words[i];
 
                 magic(word, function (result) {
-                    var status = result ? 1 : 0;   
+                    var status;
+                    if(result) {
+                        status = 1;
+                        validWords++;
+                    } else {
+                        status = 0;
+                        invalidWords++;
+                    }
                     resultWords.push({
                         word: word,
                         status: status
                     });
                     if (resultWords.length === words.length) {
                         res.status(200, { "Access-Control-Allow-Origin": "*" })
-                        .json({words: resultWords});
+                        .json({words: resultWords, validWords: validWords, invalidWords: invalidWords});
                     }
                 });              
             })(i);
